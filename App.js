c4a91a25de9ee53ee3e88231bbc79ee51529c5e1
Pena2k <t2rape00@students.oamk.fi>
2024-09-08 18:03:30 +0300

    valmis tehtävä

--- App.js
+++ App.js
@@ -1,20 +1,71 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, {useEffect, useState} from 'react';
+import {Modal, StyleSheet, Text, Pressable, View, BackHandler, Platform} from 'react-native';
 
-export default function App() {
+const App = () => {
+  const [modalVisible, setModalVisible] = useState(false);
+
+  useEffect(() => {
+    const backAction = () => {
+      if (modalVisible) {
+        setModalVisible(false);
+        return true;
+      }
+      return false;
+    };
+
+    if (Platform.OS === 'android') {
+      const backHandler = BackHandler.addEventListener('hardwareBackPress', backAction);
+      return () => backHandler.remove();
+    }
+  }, [modalVisible]);
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Modal
+        transparent={true}
+        visible={modalVisible}
+        onRequestClose={() => setModalVisible(false)}>
+        <View style={styles.modalContainer}>
+          <View style={styles.modalContent}>
+            <Text>PÖÖÖ</Text>
+            {Platform.OS === 'ios' && (
+              <Pressable onPress={() => setModalVisible(false)}>
+                <Text style={styles.buttonText}>Piilota</Text>
+              </Pressable>
+            )}
+          </View>
+        </View>
+      </Modal>
+      <Pressable onPress={() => setModalVisible(true)}>
+        <Text style={styles.buttonText}>Kurkkaa jos uskallat</Text>
+      </Pressable>
     </View>
   );
-}
+};
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
     justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#42c5ed'
+  },
+  modalContainer: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: 'rgba(0, 0, 0, 0.5)',
+  },
+  modalContent: {
+    backgroundColor: 'white',
+    padding: 20,
+    borderRadius: 10,
+    alignItems: 'center',
+  },
+  buttonText: {
+    color: '#c42121',
+    marginTop: 10,
   },
 });
+
+export default App;
